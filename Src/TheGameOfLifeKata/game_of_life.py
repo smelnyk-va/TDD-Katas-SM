4080e32c9ece7413833bea20bab5918acc65fd9d
Stephanie Melnyk <smelnyk@vendasta.com>
2016-06-22 12:46:41 -0600

    Add files via upload

---
+++ Src/TheGameOfLifeKata/game_of_life.py
@@ -0,0 +1,4 @@
+__author__ = 'smelnyk'
+
+# http://codingdojo.org/cgi-bin/index.pl?action=browse&diff=1&id=KataGameOfLife
+
---
+++ Src/TheGameOfLifeKata/game_of_life_test.py
@@ -0,0 +1,11 @@
+__author__ = 'smelnyk'
+
+import unittest
+
+# http://codingdojo.org/cgi-bin/index.pl?action=browse&diff=1&id=KataGameOfLife
+
+
+class GameOfLifeTests(unittest.TestCase):
+
+    def my_first_test(self):
+        pass
